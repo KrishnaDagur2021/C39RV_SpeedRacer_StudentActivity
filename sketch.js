a480f331e31b701f4d9d58017209ce3b350e4b9c
KrishnaDagur2021 <90098220+KrishnaDagur2021@users.noreply.github.com>
2021-11-08 19:51:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,81 @@
-var canvas;
-var backgroundImage, car1_img, car2_img, track;
-var database, gameState;
-var form, player, playerCount;
-var allPlayers, car1, car2;
-var cars = [];
+var balloon,balloonImage1,balloonImage2;
+var database;
+var height;
 
-function preload() {
-  backgroundImage = loadImage("./assets/background.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/track.jpg");
-}
+function preload(){
+   bg =loadImage("Images/cityImage.png");
+   balloonImage1=loadAnimation("Images/HotAirBallon01.png");
+   balloonImage2=loadAnimation("Images/HotAirBallon01.png","Images/HotAirBallon01.png",
+   "Images/HotAirBallon01.png","Images/HotAirBallon02.png","Images/HotAirBallon02.png",
+   "Images/HotAirBallon02.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png","Images/HotAirBallon03.png");
+  }
 
+//Function to set initial environment
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+
+   database=firebase.database();
+
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,650,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
+
+
+
+  textSize(20); 
 }
 
+// function to display UI
 function draw() {
-  background(backgroundImage);
-  if (playerCount === 2) {
-    game.update(1);
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale=balloon.scale+0.005;
   }
 
-  if (gameState === 1) {
-    game.play();
-  }
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+ function updateHeight(x,y){
+   database.ref('balloon/height').set({
+     'x': height.x + x ,
+     'y': height.y + y
+   })
+ }
+
+
+
+ function readHeight(data){
+   height = data.val();
+   balloon.x = height.x;
+   balloon.y = height.y;
+ }
+
+
+
+function showError(){
+  console.log("Error in writing to the database");
 }
